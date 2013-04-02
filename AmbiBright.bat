7cca3d28159a9890ba3c148fb3eb34810079624b
Nicolas Morel <nmr.morel@gmail.com>
2013-04-02 17:08:05 +0200

    packaging with maven

--- AmbiBright.bat
+++ AmbiBright.bat
@@ -1,3 +1,3 @@
 set folder=%~dp0
-start javaw -Xmx128M -Djava.library.path=%folder%lib -cp %folder%AmbiBright.jar;%folder%lib\* ambibright.Launcher
+start javaw -Xmx128M -cp %folder%AmbiBright.jar;%folder%lib\* ambibright.Launcher
 
